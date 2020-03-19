23cec7315bafe7641dc4a3b6de0e47d876a46982
syeyoung <cyoung06@naver.com>
2020-03-18 23:47:05 -0400

    Edit Server Ping

--- src/main/java/kr/minecheat/mcauth/handler/ServerPingHandler.java
+++ src/main/java/kr/minecheat/mcauth/handler/ServerPingHandler.java
@@ -1,11 +1,13 @@
 package kr.minecheat.mcauth.handler;
 
 import kr.minecheat.mcauth.mcdata.Chat;
+import kr.minecheat.mcauth.mcdata.ChatColor;
 import kr.minecheat.mcauth.mcdata.ServerListPingResponse;
 import kr.minecheat.mcauth.netty.MinecraftPacketHandler;
 import kr.minecheat.mcauth.packets.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ServerPingHandler extends PacketHandler {
 
@@ -28,14 +30,19 @@ public class ServerPingHandler extends PacketHandler {
                     PacketStatus00ServerListPingResponse p0slpr = new PacketStatus00ServerListPingResponse();
 
                     ServerListPingResponse slpr = new ServerListPingResponse();
-                    slpr.setVersion(new ServerListPingResponse.VersionData("Fancy Server", 47));
+                    slpr.setVersion(new ServerListPingResponse.VersionData("MineCheat-MCAuth", 47));
                     slpr.setPlayers(new ServerListPingResponse.PlayerListData.Builder()
                                             .setMax(999)
                                             .setOnline(1)
                                             .build());
                     slpr.setFavicon("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAE0lEQVR42mNk+P//PwMaYKSBIACCTQ73jQGOiQAAAABJRU5ErkJggg==");
-                    slpr.setDescription(new Chat.Builder()
-                                    .setText("Sample motd").build());
+
+                    slpr.setDescription(new Chat.Builder().setText(" \n").setExtra(Arrays.asList(
+                            new Chat.Builder().setText("           MineCheat").setColor(ChatColor.LIGHT_GREEN).build(),
+                            new Chat.Builder().setText(" 마인크래프트 계정 ").setColor(ChatColor.LIGHT_CYAN).build(),
+                            new Chat.Builder().setText("인증").setColor(ChatColor.LIGHT_RED).build(),
+                            new Chat.Builder().setText(" 서버").setColor(ChatColor.LIGHT_YELLOW).build()
+                    )).build());
                     slpr.setModinfo(new ServerListPingResponse.ModInfo(new ArrayList<>(), "VANILLA"));
 
                     p0slpr.setResponse(slpr);
